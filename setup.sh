b2b25e7bcd9ffc6581e046e1e82ead50235967f2
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2020-12-18 09:26:41 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,11 +10,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 ## INSTALL BREW PACKAGES
 # optional - python gradle maven
-brew install ffmpeg gradle jupyterlab maven starship youtube-dl
-
-## INSTALL CASKS
-brew cask install android-studio duet github google-chrome notion oracle-jdk quip slack spotify visual-studio-code vmware-fusion zoomus
-
-## INSTALL CASK FONTS
-brew tap homebrew/cask-fonts
-brew cask install font-source-code-pro font-hack-nerd-font
+brew install $(cat brews)
