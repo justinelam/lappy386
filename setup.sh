e14d79599007714581e9819e50a52c24700400b6
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2022-08-04 21:52:19 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ cat dot-zshrc >> ~/.zshrc
 
 ## INSTALL NVM
 ## https://github.com/nvm-sh/nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 ## INSTALL HOMEBREW
 ## https://brew.sh
